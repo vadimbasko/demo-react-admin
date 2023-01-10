ea661ded7c5f77acd797df7e10c2b15d0c365d65
Vadim Basko <vadim.p.basko@gmail.com>
2023-01-10 18:17:09 +0400

    demo: start

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-// in src/App.tsx
 import * as React from 'react';
-import {Admin, ListGuesser, Resource} from 'react-admin';
-import strapiRestProvider from './dataProvider';
-import {CompanyList} from "./CompanyList";
-
-// change the URL to your Strapi API
-const dataProvider = strapiRestProvider('http://localhost:1337/api');
+import {Admin} from 'react-admin';
 
 const App = () => (
-  <Admin
-    dataProvider={dataProvider}
-  >
-    <Resource name="companies" list={CompanyList} />
-    <Resource name="sys-users" list={ListGuesser} />
-  </Admin>);
+  <Admin></Admin>);
 
 export default App;
