b6611a50d8a46bf8bba43a1365839b067812919b
Vadim Basko <vadim.p.basko@gmail.com>
2023-01-10 22:43:41 +0400

    demo: step1 - dataProvider + company with guessers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
-import {Admin} from 'react-admin';
+import {Admin, EditGuesser, ListGuesser, Resource} from 'react-admin';
+
+import strapiRestProvider from './dataProvider';
+
+
+// change the URL to your Strapi API
+const dataProvider = strapiRestProvider('http://localhost:1337');
 
 const App = () => (
-  <Admin></Admin>);
+  <Admin dataProvider={dataProvider}>
+    <Resource name="companies" list={ListGuesser} edit={EditGuesser}/>
+  </Admin>);
 
 export default App;
